1bee69f3b547e2372c8e0735583bb36d065adcae
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-04-01 00:50:18 +0200

    minor format

--- src/osparc_function_services/cli.py
+++ src/osparc_function_services/cli.py
@@ -1,10 +1,10 @@
 import typer
+
 from ._utils import run_as_service
 from .sensitivity_ua_services import (
-    sensitivity_ua_test_func,
     sensitivity_ua_linear_regression,
+    sensitivity_ua_test_func,
 )
-
 
 main = typer.Typer()
 
