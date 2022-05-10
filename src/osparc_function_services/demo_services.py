d7bfeb08d949e3464e413739a291f75fba84a097
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-05-10 14:38:24 +0200

    cleanup demo func and annotate Complex type

--- src/osparc_function_services/demo_services.py
+++ src/osparc_function_services/demo_services.py
@@ -1,17 +1,21 @@
-from numbers import Complex
-from pydantic import BaseModel
-from typing import Tuple
 import time
+from numbers import Complex
+from typing import Tuple
+
+from pydantic import BaseModel, Field
+
+# Units validated against https://github.com/hgrecco/pint/blob/master/pint/default_en.txt
 
 
 class Complex(BaseModel):
-    real: float = 0.0
-    imag: float = 0.0
+    real: float = Field(0.0, description="This is the real part")
+    imag: float = Field(0.0, ge=0.0)
 
 
+def demo_func(
+    *, index: int, real_value: float, complex_value: Complex
+) -> Tuple[int, float, Complex]:
 
-
-def demo_func(*, index: int, real_value: float, complex_value: Complex) -> Tuple[int, float, Complex]:
     print("Sleeping 1.0")
     time.sleep(secs=1.0)
 
